a2c6d8956e943b96404db09b9d837e88bcbe9a62
root <antoine.dupre@maxiv.lu.se>
2018-05-25 14:28:50 +0200

    First commit

---
+++ sardana2xls/main.py
@@ -0,0 +1,153 @@
+from utils import get_elements, generate_aliases_mapping
+from utils import generate_id_mapping, generate_prop_mapping
+from utils import generate_class_mapping
+import tango
+import xlrd
+from xlutils.copy import copy
+from functools import partial
+
+pool_name = "B11*"
+db = tango.Database()
+elements = get_elements(pool_name, db)
+aliases = generate_aliases_mapping(elements, db)
+ids = generate_id_mapping(elements, db)
+ctrl_ids = generate_prop_mapping(elements, db, 'ctrl_id')
+motor_ids = generate_prop_mapping(elements, db, 'motor_role_ids')
+pseudo_ids = generate_prop_mapping(elements, db, 'pseudo_motor_role_ids')
+classes = generate_class_mapping(elements, db)
+
+
+r_workbook = xlrd.open_workbook("template.xls")
+w_workbook = copy(r_workbook)
+controllers = [k for k, v in classes.iteritems() if v == "Controller"]
+motors = [k for k, v in classes.iteritems() if v == "Motor"]
+pseudos = [k for k, v in classes.iteritems() if v == "PseudoMotor"]
+pool_name = "b110a/pool/01"
+controller_sheet = w_workbook.get_sheet(3)
+motor_sheet = w_workbook.get_sheet(4)
+pseudo_sheet = w_workbook.get_sheet(5)
+
+
+default_properties = [
+    "id",
+    "ctrl_id",
+    "motor_role_ids",
+    "pseudo_motor_role_ids",
+    "type",
+    "library",
+    "klass"]
+
+
+def get_property(ds, name):
+    return db.get_device_property(ds, name)[name][0]
+
+
+def get_property_list(name):
+    return [p for p in db.get_device_property_list(name, "*")
+            if p not in default_properties]
+
+
+def get_properties(name):
+    props = get_property_list(name)
+    return ["{}:{}".format(p, get_property(name, p)) for p in props]
+
+
+def get_controller_elements(name, ctrl_type):
+    elems = []
+    if ctrl_type == "PseudoMotor":
+        for pseudo in pseudo_ids[name]:
+            try:
+                elems.append(aliases[ids[pseudo]])
+            except KeyError as e:
+                print(e)
+    return ";".join(elems)
+
+
+def controller_data(name):
+    ctrl_prop = partial(get_property, name)
+    ctrl_type = ctrl_prop("type")
+    ctrl_lib = ctrl_prop("library")
+    ctrl_class = ctrl_prop("klass")
+    ctrl_props = ";".join(get_properties(name))
+    ctrl_elements = get_controller_elements(name, ctrl_type)
+    return [ctrl_type,
+            pool_name,
+            aliases[name],
+            ctrl_lib, ctrl_class,
+            ctrl_props,
+            ctrl_elements]
+
+
+def write_line(sheet, line, data):
+    for index, d in enumerate(data):
+        sheet.write(line, index, d)
+
+
+def proceed_controllers(names, sheet):
+    for line, controller in enumerate(names):
+        data = controller_data(controller)
+        write_line(sheet, line+1, data)
+
+
+def proceed_motors(names, sheet):
+    for line, motor in enumerate(names):
+        data = motor_data(motor, "Motor")
+        write_line(sheet, line+1, data)
+
+
+def proceed_pseudos(names, sheet):
+    for line, motor in enumerate(names):
+        data = motor_data(motor, "PseudoMotor")
+        write_line(sheet, line+1, data)
+
+
+mot_attributes = [
+    "EncoderSource",
+    "EncoderSourceFormula",
+    "Sign",
+    "Offset",
+    "Step_per_unit",
+    "UserEncoderSource"]
+
+
+def get_motor_attributes(name):
+    tango_db = tango.DeviceProxy("sys/database/2")
+    query = "Select attribute, value from property_attribute_device "
+    query += "where device='{}' and name='__value'"
+    query = query.format(name)
+    reply = tango_db.DbMySqlSelect(query)
+    reply = reply[1]
+    answer = ["{}:{}".format(att, value)
+              for att, value in zip(reply[::2], reply[1::2])]
+    return answer
+
+
+def motor_data(name, mot_type):
+    mot_type = mot_type
+    mot_pool = pool_name
+
+    mot_ctrl = aliases[ids[ctrl_ids[name][0]]]
+    try:
+        mot_alias = aliases[name]
+    except KeyError:
+        mot_alias = ""
+    mot_device = name
+    mot_axis = get_property(name, "Axis")
+    mot_instrument = ""
+    mot_desc = ""
+    mot_attributes = ";".join(get_motor_attributes(name))
+    return (mot_type,
+            mot_pool,
+            mot_ctrl,
+            mot_alias,
+            mot_device,
+            mot_axis,
+            mot_instrument,
+            mot_desc,
+            mot_attributes)
+
+
+proceed_motors(motors, motor_sheet)
+proceed_pseudos(pseudos, pseudo_sheet)
+proceed_controllers(controllers, controller_sheet)
+w_workbook.save("template2.xls")
---
+++ sardana2xls/utils.py
@@ -0,0 +1,122 @@
+import tango
+import collections
+
+
+class unique_dict(collections.MutableMapping):
+
+    def __init__(self, *args, **kwargs):
+        self._store = dict()
+        self._inverted = dict()
+        self.update(dict(*args, **kwargs))  # use the free update to set keys
+
+    def __getitem__(self, key):
+        return self._store[key]
+
+    def __setitem__(self, key, value):
+        """ issue allowed:
+            store     {1: 2, 3: 4, 5: 3}
+            inverted  {2: 1, 3: 5, 4: 3}
+        """
+        if value in self._inverted:
+            self._store.pop(self._inverted.pop(value))
+        if key in self._store and self._store[key] in self._inverted:
+            self._inverted.pop(self._store.pop(key))
+        self._inverted[value] = key
+        self._store[key] = value
+
+    def __delitem__(self, key):
+        del self._inverted[self._store[key]]
+        del self._store[key]
+
+    def __iter__(self):
+        return iter(self._store)
+
+    def __len__(self):
+        return len(self._store)
+
+    def __repr__(self):
+        return self._store.__repr__()
+
+
+class unique_bidict(unique_dict):
+    def __getitem__(self, key):
+        if key in self._store:
+            return self._store[key]
+        elif key in self._inverted:
+            return self._inverted[key]
+        else:
+            raise KeyError(key)
+
+
+# get sardana element
+# create between id and devices
+# create a map between device and alias
+
+
+def get_elements(instance, db):
+    names = db.get_device_name("Pool/{}".format(instance), "*")
+    return [name for name in names if 'dserver' not in name]
+
+
+def generate_id_mapping(devices, db):
+    id_map = unique_bidict()
+    for name in devices:
+        prop = db.get_device_property(name, 'id')['id']
+        if not prop:
+            continue
+        id_map[prop[0]] = name
+    return id_map
+
+
+def generate_aliases_mapping(devices, db):
+    alias_map = unique_bidict()
+    for name in devices:
+        try:
+            alias = db.get_alias(name)
+        except tango.DevFailed:
+            continue
+        alias_map[alias] = name
+    return alias_map
+
+
+def generate_prop_mapping(devices, db, prop_name):
+    id_map = dict()
+    for name in devices:
+        prop = db.get_device_property(name, prop_name)[prop_name]
+        if not prop:
+            continue
+        id_map[name] = prop
+    return id_map
+
+
+def generate_class_mapping(devices, db):
+    return {d: db.get_class_for_device(d) for d in devices}
+
+
+if __name__ == "__main__":
+    db = tango.Database()
+    elements = get_elements("B110A-TMP", db)
+    aliases = generate_aliases_mapping(elements, db)
+    ids = generate_id_mapping(elements, db)
+    ctrl_ids = generate_prop_mapping(elements, db, 'ctrl_id')
+    motor_ids = generate_prop_mapping(elements, db, 'motor_role_ids')
+    pseudo_ids = generate_prop_mapping(elements, db, 'pseudo_motor_role_ids')
+    # import networkx as nx
+    # import matplotlib.pyplot as plt
+    # import pydot
+    # G = nx.Graph()
+    # G.add_nodes_from(ids.values()
+    #                  )
+
+    # def add_edge(iterable):
+    #     for name, elems in iterable.items():
+    #         for e in elems:
+    #             G.add_edge(name, ids[e])
+    # add_edge(motor_ids)
+    # add_edge(pseudo_ids)
+    # add_edge(ctrl_ids)
+
+    # # nx.draw(G, node_size=100, cmap=plt.cm.Blues,
+    #         node_color=range(len(G)),
+    #         prog='dot',
+    #         with_labels=True)
