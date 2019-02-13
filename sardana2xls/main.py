c9fb00962e071e857fc753ba0ec87fc54a59a8f2
antdup <antoine.dupre@maxiv.lu.se>
2019-02-13 18:29:46 +0100

    add IORegister and Channels

--- sardana2xls/main.py
+++ sardana2xls/main.py
@@ -16,7 +16,7 @@ logging.basicConfig(level=logging.DEBUG)
 db = tango.Database()
 
 # Setup
-pool = "Femtomax"
+pool = "FemtoMAX"
 pool_server = "Pool/{}".format(pool)
 pool_name = db.get_device_name(pool_server, "Pool")[0]
 logging.info("Pool: {}".format(pool))
@@ -39,6 +39,12 @@ classes = generate_class_mapping(elements, db)
 controllers = [k for k, v in classes.items() if v == "Controller"]
 motors = [k for k, v in classes.items() if v == "Motor"]
 pseudos = [k for k, v in classes.items() if v == "PseudoMotor"]
+iors = [k for k, v in classes.items() if v == "IORegister"]
+channels = [
+    (k, v)
+    for k, v in classes.items()
+    if "counter" in v.lower() or "channel" in v.lower()
+]
 
 
 # Open xls file
@@ -49,6 +55,9 @@ motor_sheet = w_workbook.get_sheet(4)
 pseudo_sheet = w_workbook.get_sheet(5)
 pool_sheet = w_workbook.get_sheet(1)
 global_sheet = w_workbook.get_sheet(0)
+ior_sheet = w_workbook.get_sheet(6)
+channel_sheet = w_workbook.get_sheet(7)
+measurment_sheet = w_workbook.get_sheet(8)
 
 
 default_properties = [
@@ -114,6 +123,7 @@ def write_line(sheet, line, data):
 
 
 def proceed_controllers(names, sheet):
+    logging.info("Create controllers")
     ctrls = []
     for ctrl in names:
         data = controller_data(ctrl)
@@ -124,6 +134,7 @@ def proceed_controllers(names, sheet):
 
 
 def proceed_motors(names, sheet):
+    logging.info("Create motors")
     motors = []
     for motor in names:
         data = motor_data(motor, "Motor")
@@ -134,6 +145,7 @@ def proceed_motors(names, sheet):
 
 
 def proceed_pseudos(names, sheet):
+    logging.info("Create pseudo motors")
     pseudos = []
     for motor in names:
         data = motor_data(motor, "PseudoMotor")
@@ -194,6 +206,74 @@ def motor_data(name, mot_type):
     )
 
 
+def ior_data(name):
+    ior_ctrl = aliases[ids[ctrl_ids[name][0]]]
+    ior_type = "IORegister"
+    ior_pool = pool_name
+    ior_alias = aliases[name]
+    ior_name = name
+    ior_axis = get_property(name, "Axis")
+    ior_instrument = ""
+    ior_desc = ""
+    ior_attributes = ";".join(get_motor_attributes(name))
+    return (
+        ior_type,
+        ior_pool,
+        ior_ctrl,
+        ior_alias,
+        ior_name,
+        ior_axis,
+        ior_instrument,
+        ior_desc,
+        ior_attributes,
+    )
+
+
+def proceed_iors(names, sheet):
+    logging.info("Create ioregister")
+    _iors = []
+    for ior in names:
+        data = ior_data(ior)
+        _iors.append(data)
+    _iors = sorted(_iors, key=lambda x: (x[2], x[5]))
+    for line, data in enumerate(_iors):
+        write_line(sheet, line + 1, data)
+
+
+def channel_data(name, _type):
+    channel_ctrl = aliases[ids[ctrl_ids[name][0]]]
+    channel_type = _type
+    channel_pool = pool_name
+    channel_alias = aliases[name]
+    channel_name = name
+    channel_axis = get_property(name, "Axis")
+    channel_instrument = ""
+    channel_desc = ""
+    channel_attributes = ";".join(get_motor_attributes(name))
+    return (
+        channel_type,
+        channel_pool,
+        channel_ctrl,
+        channel_alias,
+        channel_name,
+        channel_axis,
+        channel_instrument,
+        channel_desc,
+        channel_attributes,
+    )
+
+
+def proceed_channel(names, sheet):
+    logging.info("Create channels")
+    _channels = []
+    for channel, _type in names:
+        data = channel_data(channel, _type)
+        _channels.append(data)
+    _channels = sorted(_channels, key=lambda x: (x[2], x[5]))
+    for line, data in enumerate(_channels):
+        write_line(sheet, line + 1, data)
+
+
 def proceed_pool(name, sheet):
     # get_properties
     host = ":".join((db.get_db_host(), str(db.get_db_port())))
@@ -224,5 +304,7 @@ proceed_pseudos(pseudos, pseudo_sheet)
 proceed_controllers(controllers, controller_sheet)
 proceed_pool(pool_name, pool_sheet)
 proceed_global(pool, global_sheet)
+proceed_iors(iors, ior_sheet)
+proceed_channel(channels, channel_sheet)
 
 w_workbook.save("{}.xls".format(pool))
