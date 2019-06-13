28f6992db1dbab0c50957e40ce78db35a6d1efb1
aureocarneiro <aureo.carneiro@gmail.com>
2019-06-13 14:24:56 +0200

    add setup.py for packaging

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name = "python-sardana2xls",
+      version = "1.0.0",
+      description = ("Tool to generate a xls representation of an existing Tango Sardana environment"),
+      author = "KITS Group",
+      author_mail = "kits@maxiv.lu.se",
+      license = "GPLv3",
+      url = "http://www.maxlab.lu.se",
+      packages =["sardana2xls"],
+      entry_points={
+          "console_scripts": ["sardana2xls = sardana2xls.sardana2xls:main"]},
+      install_requires=["setuptools", "pytango", "xlrd"],
+      )
